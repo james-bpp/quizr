4901fe13154c478afef09b5b768fe76735e216ab
Robert Austin <austin437@hotmail.com>
2021-05-06 15:35:26 +0100

    1.2.13

--- includes/cpts/class-quizr-question-cpt.php
+++ includes/cpts/class-quizr-question-cpt.php
@@ -58,35 +58,11 @@ class Quizr_Question_Cpt {
         else 
         {
             $meta_value = get_post_meta( $post->ID, 'quizr_question_set_id', true);
-        }
-
-        
+        }      
 
         wp_nonce_field( 'quizr_question_set_id_nonce', 'quizr_question_set_id_nonce_' . $post->ID);
 
-         ?>
-        <div class="quizr-select-group-flex">
-            <select id="quizr_question_set_id" name="quizr_question_set_id" class="widefat">
-               <option value=""></option>
-               <?php foreach( $question_sets as $qs){ ?>
-                    <option 
-                        value="<?php echo esc_html( $qs->ID); ?>" 
-                        <?php echo (int) $qs->ID === (int) $meta_value ? 'selected="selected"' : ''; ?>
-                    >
-                    <?php echo esc_html( $qs->post_title); ?></option>
-               <?php } ?>
-            </select>
-            <a href="<?php echo get_edit_post_link( $meta_value ); ?>" 
-                class="button button-secondary">View</a>
-        </div>
-        <?php if( (int) $meta_value > 0 ) { ?>
-            <hr />
-            <a href="<?php echo admin_url('post-new.php?post_type=quizr_question') . "&question_set_id=$meta_value"; ?>" >
-                Add New
-            </a>
-        <?php } ?>
-
-        <?php
+        require_once QUIZR_ADMIN_PATH . '/partials/quizr-admin-cpt-question-qsmb.php';
     }
 
     public function save_custom_meta_data( $id ){

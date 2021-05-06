8a2ea8d3f4447cf7a50bf2487977ce25c9956906
Robert Austin <austin437@hotmail.com>
2021-05-06 14:14:25 +0100

    1.2.11

--- includes/cpts/class-quizr-question-cpt.php
+++ includes/cpts/class-quizr-question-cpt.php
@@ -62,6 +62,12 @@ class Quizr_Question_Cpt {
             <a href="<?php echo get_edit_post_link( $meta_value ); ?>" 
                 class="button button-secondary">View</a>
         </div>
+        <?php if( (int) $meta_value > 0 ) { ?>
+            <hr />
+            <a href="<?php echo admin_url('post-new.php?post_type=quizr_question') . "&question_set_id=$meta_value"; ?>" >
+                Add New
+            </a>
+        <?php } ?>
 
         <?php
     }

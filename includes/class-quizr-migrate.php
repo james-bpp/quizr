b723c145f3e51f76e23b99fa9cb046a5e30a37a6
Robert Austin <austin437@hotmail.com>
2021-05-12 13:03:45 +0100

    1.0.3

--- includes/class-quizr-migrate.php
+++ includes/class-quizr-migrate.php
@@ -14,18 +14,19 @@ class Quizr_Migrate {
     }
 
     public function up(){
-        
+
         global $wpdb;
 
         $table_name = $wpdb->prefix . 'quizr_answers';
 
-        $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        $sql = "CREATE TABLE $table_name (
             id mediumint(9) NOT NULL AUTO_INCREMENT,
             quizr_question_id INT NOT NULL,
             description tinytext NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            PRIMARY KEY (id)
+            is_correct TINYINT(1) NOT NULL,
+            PRIMARY KEY  (id)
         );";
 
         require_once( ABSPATH . 'wp-admin/includes/upgrade.php');

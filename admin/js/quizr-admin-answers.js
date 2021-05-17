62071899c83f25c2bda847e6997da6abf6bf9b37
Robert Austin <austin437@hotmail.com>
2021-05-17 18:02:57 +0100

    1.3.10

--- admin/js/quizr-admin-answers.js
+++ admin/js/quizr-admin-answers.js
@@ -2,7 +2,47 @@ class Quizr_Admin_Answers {
 
     constructor( element ){
         this.element = element;
-        console.log(this.element);
+        this.add_event_listeners();
+    }
+
+    enable_element_edit(ev){
+
+        ev.preventDefault();
+        
+        const selected_row = helpers.find_parent_by_tag_name( ev.target, 'tr');
+
+        const description = selected_row.querySelectorAll('input');
+
+        for( let item of description ){
+            item.removeAttribute('readonly');
+        }
+
+    }
+
+    remove_element_from_dom(ev){
+        ev.preventDefault();
+
+        if( confirm('Are you sure you would like to delete this item?')){
+            const selected_row = helpers.find_parent_by_tag_name( ev.target, 'tr');
+
+            selected_row.remove();
+
+            alert('You will need to click the Update button to save this deletion');
+        }
+    }
+
+    add_event_listeners(){
+        const update_els = this.element.querySelectorAll('.quizr-answer-edit');
+
+        for( let el of update_els){
+            el.addEventListener( 'click', this.enable_element_edit )
+        }
+
+        const delete_els = this.element.querySelectorAll('.quizr-answer-delete');
+
+        for( let el of delete_els ){
+            el.addEventListener( 'click', this.remove_element_from_dom );
+        }
     }
 
 }

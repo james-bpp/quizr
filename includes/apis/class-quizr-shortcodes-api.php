1925d834fb86db363347eb21f97f331ce14d33e7
Robert Austin <austin437@hotmail.com>
2021-05-20 18:01:21 +0100

    1.5.2

--- includes/apis/class-quizr-shortcodes-api.php
+++ includes/apis/class-quizr-shortcodes-api.php
@@ -4,8 +4,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class Quizr_Shortcodes_Api {
 
+    public function render_quizr_question_set_html( $atts, $content = null ){
+        return '<p>This is a shortcode</p>';
+    }
+
     public function register_shortcodes(){
-        
+        add_shortcode( 'quizr_question_set', array( $this, 'render_quizr_question_set_html' ) );
     }
 
 }

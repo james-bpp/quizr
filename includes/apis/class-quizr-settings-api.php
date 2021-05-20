c3453adf359345e499f83e458f43bf0c7254f66c
Robert Austin <austin437@hotmail.com>
2021-05-20 11:23:54 +0100

    1.4.2

--- includes/apis/class-quizr-settings-api.php
+++ includes/apis/class-quizr-settings-api.php
@@ -5,7 +5,17 @@ if( ! defined( 'ABSPATH' ) ) exit;
 class Quizr_Settings_Api {
 
     public function register_settings(){
+        register_setting(
+            'quizr_options_group',
+            'quizr_max_answers_per_question',
+            array( 'type' => 'string', 'sanitize_callback' => 'santize_text_field' )
+        );
 
+        register_setting(
+            'quizr_options_group',
+            'quizr_show_cpt_question_in_menu',
+            array( 'type' => 'string', 'sanitize_callback' => 'santize_text_field' )
+        );
     }
 
     public function register_options_page(){
@@ -17,7 +27,7 @@ class Quizr_Settings_Api {
     }
 
     public function add_plugin_page_settings_link( $links ){
-        
+
     }
 
 

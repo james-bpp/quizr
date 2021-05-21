028dde84a58c295c74f6dc6db9f93e2b2fa146b2
Robert Austin <austin437@hotmail.com>
2021-05-21 15:08:54 +0100

    1.6.2

--- public/partials/quizr-public-shortcodes.php
+++ public/partials/quizr-public-shortcodes.php
@@ -1,1 +1,31 @@
-<p>This is a shortcode</p>
+<div>         
+    <div>
+        <article> 
+            <div>
+                <aside 
+                    <img src="" />   
+                </aside>   
+                <div>
+                    <header> 
+                        <h2>Time for a Quizr</h2>
+                    </header> 
+                    <section>
+                        <h3>Subject: <?php echo $question_set->post_title; ?></h3>
+                        <p>You will be shown some multiple choice questions</p>  
+                        <ul>
+                            <li>You can move backwards and forwards amongst the questions</li>
+                            <li>At the end you will see a summary of your answers</li>
+                            <li>After submitting your answers, you will get your score</li>
+                        </ul>
+                    </section>    
+                </div>
+                <div>
+                    <div>
+                        <p>When you hover over the card, the answers are revealed here!</p>
+                    </div>
+                </div>
+            </div>
+            <a href="">START QUIZ</a>
+        </article>
+    </div>
+</div>

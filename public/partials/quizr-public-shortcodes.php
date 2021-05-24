9c167994f9a387686402150d35aa30d893c006c7
Robert Austin <austin437@hotmail.com>
2021-05-24 11:13:33 +0100

    1.6.4

--- public/partials/quizr-public-shortcodes.php
+++ public/partials/quizr-public-shortcodes.php
@@ -1,11 +1,11 @@
-<div>         
-    <div>
-        <article> 
-            <div>
-                <aside>
-                    <img src="https://www.wp-plugins.dev.cc/wp-content/uploads/2021/05/quizr-logo.png" />   
+<div class="quizr-qs">         
+    <div class="quizr-qs-intro">
+        <article class="quizr-qs-card"> 
+            <div class="quizr-qs-card__container">
+                <aside class="quizr-qs-card__sidebar">
+                    <img class="quizr-qs-card__img" src="https://www.wp-plugins.dev.cc/wp-content/uploads/2021/05/quizr-logo.png" />   
                 </aside>   
-                <div>
+                <div class="quizr-qs-card__content">
                     <header> 
                         <h2>Time for a Quizr</h2>
                     </header> 
@@ -19,13 +19,13 @@
                         </ul>
                     </section>    
                 </div>
-                <div>
-                    <div>
+                <div class="quizr-qs-card__answers">
+                    <div class="quizr-qs-card__answer-label">
                         <p>When you hover over the card, the answers are revealed here!</p>
                     </div>
                 </div>
             </div>
-            <a href="">START QUIZ</a>
+            <a class="quizr-qs-intro__start-quiz" href="">START QUIZ</a>
         </article>
     </div>
 </div>
--- quizr.php
+++ quizr.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'QUIZR_VERSION', '1.6.3' );
+define( 'QUIZR_VERSION', '1.6.4' );
 
 define( 'QUIZR_DB_VERSION', '1.0.3' );
 

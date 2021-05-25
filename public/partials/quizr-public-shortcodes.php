8a10209b6f48771a68d5072382ff03e8ace327f6
Robert Austin <austin437@hotmail.com>
2021-05-25 11:20:47 +0100

    1.7.1

--- public/partials/quizr-public-shortcodes.php
+++ public/partials/quizr-public-shortcodes.php
@@ -28,4 +28,34 @@
             <a class="quizr-qs-intro__start-quiz" href="">START QUIZ</a>
         </article>
     </div>
+    <form class="quizr-form" data-id="<?php echo $qs_id; ?>">
+        <div class="quizr-qs-questions quizr-qs-hide quizr-qs-show--block">
+            <?php foreach( $questions as $key => $q ) { ?>
+                <article class="quizr-qs-card"> 
+                    <div class="quizr-qs-card__container">
+                        <aside class="quizr-qs-card__sidebar">
+                            <img class="quizr-qs-card__img" src="https://www.wp-plugins.dev.cc/wp-content/uploads/2021/05/quizr-logo.png" />   
+                        </aside>   
+                        <div class="quizr-qs-card__content">
+                            <header> 
+                                <h2>Question <?php echo $key + 1; ?></h2>
+                            </header> 
+                            <section>
+                                <h3><?php echo $q->post_title; ?></h3>
+                                <?php echo apply_filters( 'the_content', $q->post_content); ?>
+                            </section>    
+                        </div>
+                        <div class="quizr-qs-card__answers">
+                            <div class="quizr-qs-card__answer-label">
+                                <p>When you hover over the card, the answers are revealed here!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="quizr-qs-intro__start-quiz" href="">START QUIZ</a>
+                </article>
+
+            <?php } ?>
+        </div>
+    </form>
+    
 </div>
